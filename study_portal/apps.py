f5f171d1c90dbaf75ec91ded726deecd232311a8
ANAS <eng.anasalhakim@gmail.com>
2023-02-05 17:46:08 +0200

    Upload Project Files

---
+++ study_portal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudyPortalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'study_portal'
---
+++ study_portal/views.py
@@ -0,0 +1,229 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .models import *
+from .forms import RoomForm, UserForm
+
+def loginPage(request):
+
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')    
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR Password incorrect')    
+
+
+    context = {'page':page}
+    return render(request, 'study_portal/login.html', context)
+
+def registerPage(request):
+
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occured during registraion")     
+
+    context = {'form':form}
+    return render(request, 'study_portal/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def home(request):
+
+    q = request.GET.get('q') if request.GET.get('q') != None else '' 
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) 
+    )
+
+    room_count = rooms.count()
+
+    topics = Topic.objects.all()[0:5]
+
+    #room_messages = Message.objects.all().order_by('-created')
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains = q)).order_by('-created')
+
+    context = {'rooms':rooms, 'topics':topics,
+               'room_count':room_count, 'room_messages':room_messages}    
+    return render(request, 'study_portal/home.html', context)
+
+def room(request, pk):
+
+    room = Room.objects.get(id=pk)
+
+    room_messages = room.message_set.all().order_by('-created')
+
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room' , pk = room.id)
+
+    context = {'room':room, 'room_messages':room_messages, 'participants':participants}
+    return render(request, 'study_portal/room.html', context)
+
+@login_required(login_url='login')
+def userProfile(request,pk):
+
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all().order_by('-created')
+    topics = Topic.objects.all()
+
+    context = {'user':user, 'rooms':rooms,
+               'room_messages':room_messages, 'topics':topics}
+
+    return render(request, 'study_portal/profile.html', context)
+
+
+
+@login_required(login_url='login')
+def createRoom(request):
+
+    form = RoomForm()
+    topics = Topic.objects.all()
+
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
+        #form = RoomForm(request.POST)
+
+        #if form.is_valid():
+            #room = form.save(commit=False)
+            #room.host = request.user
+            #room.save()
+            #return redirect('home') 
+
+    context = {'form':form, 'topics':topics}    
+    return render(request, 'study_portal/room_form.html', context)
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    topics = Topic.objects.all()
+
+    if request.user != room.host:
+        return HttpResponse("You're not allowed!")
+
+    if request.method == 'POST':
+
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+
+        #form = RoomForm(request.POST, instance=room)
+        #if form.is_valid():
+            #form.save()
+            #return redirect('home') 
+
+    context = {'form':form, 'topics':topics, 'room':room}
+    return render(request, 'study_portal/room_form.html', context)
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse("You're not allowed!")
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'obj': room}
+    return render(request, 'study_portal/delete.html', context)
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("You're not allowed!")
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', pk= message.room.id)
+
+    context = {'obj': message}
+    return render(request, 'study_portal/delete.html', context)    
+
+
+@login_required(login_url='login')
+def updateUser(request):
+
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form':form}
+    return render(request, 'study_portal/update-user.html', context)    
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'study_portal/topics.html', {'topics': topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'study_portal/activity.html', {'room_messages': room_messages})
